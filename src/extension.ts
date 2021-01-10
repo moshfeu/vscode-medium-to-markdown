fc5e2fda96c93786d3c50ebec3ffc00a72087e13
Mosh Feu <moshfeu@gmail.com>
2021-01-10 10:49:50 +0200

    download path + file type options

--- src/extension.ts
+++ src/extension.ts
@@ -13,7 +13,8 @@ export function activate(context: vscode.ExtensionContext) {
     async () => {
       const url = await vscode.window.showInputBox({
         placeHolder: 'Medium URL',
-        value: 'https://medium.com/swlh/build-your-own-analytics-tool-on-corvid-by-wix-cd12ae76d291',
+        value:
+          'https://medium.com/swlh/build-your-own-analytics-tool-on-corvid-by-wix-cd12ae76d291',
         validateInput: (input) => {
           try {
             new URL(input);
@@ -24,32 +25,52 @@ export function activate(context: vscode.ExtensionContext) {
         },
       });
 
-
       if (url) {
-        await vscode.window.withProgress({
-          title: 'Fetching...',
-          location: vscode.ProgressLocation.Notification,
-        }, async () => {
-          const postContent = await mediumToMarkdown.convertFromUrl(url);
-          if (postContent) {
-            try {
-              const { title, content } = getPostDetails(postContent);
-              const postName = getFileName(title);
-              await new Promise((resolve) => {
-                const path = join(vscode.workspace.workspaceFolders[0].uri.fsPath, `${postName}.md`);
-                writeFile(
-                  path,
-                  content,
-                  resolve
-                )
-              });
-            } catch (error) {
-              console.log(error);
-            }
+        await vscode.window.withProgress(
+          {
+            location: vscode.ProgressLocation.Notification,
+          },
+          async (progress) => {
+            return new Promise(async (progressResolve, progressReject) => {
+              progress.report({ message: 'Fetching...' });
+              const postContent = await mediumToMarkdown.convertFromUrl(url);
+              if (postContent) {
+                try {
+                  const { title, content } = getPostDetails(postContent);
+                  const postName = getFileName(title);
+                  const {
+                    downloadPath,
+                    fileType,
+                  } = vscode.workspace.getConfiguration('mediumToMarkdown');
+                  const postPath = await new Promise((resolve) => {
+                    const postPath = join(
+                      downloadPath,
+                      `${postName}.${fileType}`
+                    );
+                    const path = join(
+                      vscode.workspace.workspaceFolders[0].uri.fsPath,
+                      postPath
+                    );
+                    writeFile(path, content, () => resolve(postPath));
+                  });
+                  progress.report({
+                    message: `Done. Post saved to ${postPath}`,
+                    increment: 100,
+                  });
+                  setTimeout(() => {
+                    progressResolve();
+                  }, 3000);
+                } catch (error) {
+                  progressReject(error);
+                }
+              } else {
+                progressReject(
+                  `The post has no content, please double check the URL. It it's good, please open an issue`
+                );
+              }
+            });
           }
-        });
-
-        vscode.window.showInformationMessage('Done');
+        );
       }
     }
   );
--- test.js
+++
@@ -1,3 +0,0 @@
-const kc = require('kebab-case');
-const title = 'Build Your Own Analytics Tool on Corvid by Wix';
-console.log(title.replace(/ /g, '-').toLowerCase());
